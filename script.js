27dc1648bfee0707f2fe6e75524f56f881a2643b
kKrishGupta <kg20060220@gmail.com>
2025-10-20 11:04:46 +0530

    added all html,css and javascript code

--- script.js
+++ script.js
@@ -1,1 +1,44 @@
-let btn = document.querySelector("button")
+let btn = document.querySelector("button");
+const output = document.getElementById("output");
+
+const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+const recognition = new SpeechRecognition();
+
+btn.addEventListener('click',() =>{
+recognition.start();
+});
+
+recognition.onresult = (event)=>{
+  const command = event.results[0][0].transcript.toLowerCase();
+  output.innerText = "You said "+command;
+  takeCommand(command);
+};
+
+function takeCommand(message){
+  if(message.includes("hello")){
+    speak("Hello Krish! How can I help you today");
+  }else if(message.includes("time")){
+    let time = new Date().toLocaleTimeString();
+    speak("The time is "+time);
+  }else if(message.includes("open youtube")){
+    speak("Opening Youtube");
+    window.open("https://www.youtube.com");
+  }
+  else if(message.includes("open google")){
+    speak("Opening Google");
+    window.open("https://www.google.com");
+  }
+  else{
+    speak("Sorry, I didn't understand that.");
+  }
+}
+
+// Text-to-speech
+function speak(text){
+  let utterance = new SpeechSynthesisUtterance(text);
+  utterance.rate = 1;
+  utterance.pitch = 1;
+  utterance.volume = 1;
+  utterance.lang = 'en-GB';
+  window.speechSynthesis.speak(utterance);
+}
